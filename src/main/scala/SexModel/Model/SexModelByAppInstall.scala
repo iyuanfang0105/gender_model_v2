28881b5fb9e96995969dd5686169295f2c48340e
iyuanfang0105@gmail.com <Fang1986>
2017-10-26 15:13:17 +0800

    using spark metric to evaluate performance

--- src/main/scala/SexModel/Model/SexModelByAppInstall.scala
+++ src/main/scala/SexModel/Model/SexModelByAppInstall.scala
@@ -58,9 +58,7 @@ object SexModelByAppInstall {
     calendar.set(year,month-1,day)
     val yestoday_Date: String = new SimpleDateFormat("yyyyMMdd").format(calendar.getTime)
 
-
     val splitChar: String = "\u0001"
-    val model_save_dir: String = "hdfs://mzcluster/user/mzsip/yuanfang/algo/sex_model/models/app_install/" + yestoday_Date + "/"
 
     // sex knonwn data
     val sex_know_table_name: String = "algo.yf_imei_sex_for_build_sexmodel_flyme"
@@ -68,28 +66,25 @@ object SexModelByAppInstall {
     val topK: Int = 30000
     // app install feature by imei
     val imei_feature_table_name: String = "algo.yf_sex_model_imei_app_install_features_on_" + topK.toString + "dims"
-    // val create_predict_table_name: String = "algo.yf_sex_model_app_install_predict_on_" + topK.toString + "dims"
-    val create_predict_table_name: String = "algo.yf_sex_model_app_install_predict_on_" + topK.toString + "_new_2"
+    val create_predict_table_name: String = "algo.yf_sex_model_app_install_predict_on_" + topK.toString + "_new_1"
 
     // prepare data and train model
     val dim_num: Int = topK
     val train_pred_set: (RDD[(String, LabeledPoint)], RDD[(String, LabeledPoint)]) = getTrainSet_predictSet(hiveContext, sex_know_table_name, imei_feature_table_name, dim_num, splitChar, yestoday_Date)
-    val model: (LogisticRegressionModel, RDD[(String, (Double, Double))]) = build_model_1(train_pred_set, 2)
-
+    val model: (LogisticRegressionModel, RDD[(String, (Double, Double))]) = build_model(train_pred_set, 2)
     report_model_performance(model._2)
+
     // predict on data set of imei
     val pred: RDD[(String, String)] = predict(model._1, train_pred_set)
-
+    // store the result
     import hiveContext.implicits._
     val pre_df: DataFrame = pred.repartition(100).map(v => Imei_sex(v._1, v._2)).toDF
-
     println("\n\n ********************* (Strarting)Insert result to yf table *********************\n\n ")
     pre_df.registerTempTable("prediction")
     hiveContext.sql(
       "create table if not exists " +
         create_predict_table_name +
         " (imei string, sex string) partitioned by (stat_date string) stored as textfile")
-
     hiveContext.sql(
       "insert overwrite table " +
         create_predict_table_name +
@@ -98,21 +93,21 @@ object SexModelByAppInstall {
         " ) select * from prediction")
     println("\n\n ********************* (Done)Insert result to yf table *********************\n\n ")
 
-//    // insert the predict result to latest table, and another job will write the result
-//    // to final sex table
-//    println("\n\n ********************* (Strarting)Insert result to final table *********************\n\n ")
-//    val sex_last_pre_tableName: String = "algo.sex_model_latest_predictionAll"
-//    val create_sex_last_pre_tableSql: String =
-//      "create table if not exists " +
-//        sex_last_pre_tableName +
-//        " (imei string, sex string) stored as textfile"
-//    val insert_sex_last_pre_tableSql: String =
-//      "insert overwrite table " +
-//        sex_last_pre_tableName +
-//        " select * from prediction"
-//    hiveContext.sql(create_sex_last_pre_tableSql)
-//    hiveContext.sql(insert_sex_last_pre_tableSql)
-//    println("\n\n ********************* (Done)Insert result to final table *********************\n\n ")
+    // insert the predict result to latest table, and another job will write the result
+    // to final sex table
+    println("\n\n ********************* (Strarting)Insert result to final table *********************\n\n ")
+    val sex_last_pre_tableName: String = "algo.sex_model_latest_predictionAll"
+    val create_sex_last_pre_tableSql: String =
+      "create table if not exists " +
+        sex_last_pre_tableName +
+        " (imei string, sex string) stored as textfile"
+    val insert_sex_last_pre_tableSql: String =
+      "insert overwrite table " +
+        sex_last_pre_tableName +
+        " select * from prediction"
+    hiveContext.sql(create_sex_last_pre_tableSql)
+    hiveContext.sql(insert_sex_last_pre_tableSql)
+    println("\n\n ********************* (Done)Insert result to final table *********************\n\n ")
   }
 
   def predict(model: LogisticRegressionModel, train_pre: (RDD[(String, LabeledPoint)], RDD[(String, LabeledPoint)])): RDD[(String, String)] = {
@@ -126,16 +121,6 @@ object SexModelByAppInstall {
     pre
   }
 
-  /**
-   * 将用户的的行为数据整理成两部分数据，一部分是性别标签已知的样本，一部分是性别标签位置的样本
-   * @param hiveContext                用于读取hive库中的表
-   * @param know_sex_table_name        性别标签已知的imei，格式是: (imei, sex)  sex=1表示男   sex=0表示女
-   * @param imei_feature_table_name    用户的行为数据，格式是：（imei, feature)
-   * @param dim_num                    用户行为对应的维度
-   * @param splitChar                  字段临时分隔符
-   * @return                           返回两部分数据，一个是标签位置的样本，一个是标签已知的样本，
-   *                                   用标签已知的样本建立模型，然后预测标签位置的样本
-   */
   def getTrainSet_predictSet(
                               hiveContext: HiveContext,
                               know_sex_table_name: String,
@@ -184,116 +169,7 @@ object SexModelByAppInstall {
     (trainSet, predictSet)
   }
 
-  def deleteIfexists(model_save_dir: String): Unit = {
-    val conf: Configuration = new Configuration()
-    val fs: FileSystem = FileSystem.get(URI.create(model_save_dir), conf, "mzsip")
-    val path: Path = new Path(model_save_dir)
-    if (fs.exists(path))
-      fs.delete(path, true)
-  }
-
-  /**
-   * 将标签已知的数据分为训练集和测试集，训练集用于建立模型，测试集用于检验模型效果
-   * @param sc                       当前的spark运行上下文
-   * @param trainSet                 标签已知的样本
-   * @param model_save_dir           模型的保存路径
-   * @return                         返回一个logistic二分类模型和这个模型的对应的分类阈值
-   */
-  def buildModel(sc: SparkContext, trainSet: RDD[(String, LabeledPoint)], model_save_dir: String): (Double, LogisticRegressionModel) = {
-    deleteIfexists(model_save_dir)
-    var ret_model: LogisticRegressionModel = null
-    val rdds: Array[RDD[(String, LabeledPoint)]] = trainSet.randomSplit(Array(0.8, 0.2))
-    var trainRDD: RDD[(String, LabeledPoint)] = rdds(0).cache()
-    val predictRDD: RDD[(String, LabeledPoint)] = rdds(1).cache()
-    val num_1: Long = predictRDD.filter(_._2.label == 1d).count()
-    val num_0: Long = predictRDD.filter(_._2.label == 0d).count()
-
-    val thresholds: Array[Double] = (for (i <- 1 until 100) yield i.toDouble / 100).toArray
-    val split: String = "\t"
-    var model: LogisticRegressionModel = null
-    var prediction: RDD[(String, (Double, Double))] = sc.emptyRDD[(String, (Double, Double))]
-    var maxVar: Double = 0
-    var goodthreshold: Double = 0
-    var threshold: Double = 0
-    val iterationNum: Int = 10
-    var k: Int = 0
-    while (k < iterationNum) {
-      var predictInfo: ArrayBuffer[String] = new ArrayBuffer[String]()
-      val train1: Long = trainRDD.filter(_._2.label == 1d).count()
-      val train0: Long = trainRDD.filter(_._2.label == 0d).count()
-      val train_info: String = "train1: " + train1 + split + "train0: " + train0
-      predictInfo += train_info
-      var tmpVariance: Double = 0
-      model = new LogisticRegressionWithLBFGS().run(trainRDD.map(_._2))
-      model.clearThreshold()
-      model.save(sc, model_save_dir + "model_" + k.toString + "/model")
-      prediction = predictRDD.map(v => (v._1, (v._2.label, model.predict(v._2.features)))).cache()
-      println(s"\n\n&&&&&&&&&&&&&&&&&  第 $k 次模型效果！！ &&&&&&&&&&&&&&&&&")
-      println(train_info)
-      for (i <- 0 until thresholds.length) {
-        val num_11: Long = prediction.filter(v => v._2._1 == 1d && v._2._2 > thresholds(i)).count()
-        val num_10: Long = prediction.filter(v => v._2._1 == 1d && v._2._2 <= thresholds(i)).count()
-        val num_00: Long = prediction.filter(v => v._2._1 == 0d && v._2._2 <= thresholds(i)).count()
-        val num_01: Long = prediction.filter(v => v._2._1 == 0d && v._2._2 > thresholds(i)).count()
-        val pre1: Double = if (num_11 + num_01 == 0d) 0d else num_11.toDouble / (num_11 + num_01)
-        val pre0: Double = if (num_00 + num_10 == 0d) 0d else num_00.toDouble / (num_00 + num_10)
-        val cov1: Double = if (num_1 == 0) 0d else num_11.toDouble / num_1
-        val cov0: Double = if (num_0 == 0) 0d else num_00.toDouble / num_0
-        val variance: Double = math.sqrt(cov1 * cov0)
-        if (variance > tmpVariance) {
-          tmpVariance = variance
-          goodthreshold = thresholds(i)
-        }
-        val info: String =
-          "threshold: " + thresholds(i) + split +
-          "num_11:" + num_11 + split +
-          "num_10: " + num_10 + split +
-          "num_00: " + num_00 + split +
-          "num_01: " + num_01 + split +
-          "pre1: " + pre1 + split +
-          "pre_0: " + pre0 + split +
-          "cov1: " + cov1 + split +
-          "cov0: " + cov0
-        predictInfo += info
-        // println(info)
-      }
-      val pre_info: String =
-        "num_1: " + num_1 + split +
-        "num_0: " + num_0 + split +
-        "tmpVariance: " + tmpVariance + split +
-        "goodthreshold: " + goodthreshold
-      predictInfo += pre_info
-      // println(pre_info)
-
-      sc.parallelize(predictInfo.toSeq)
-        .repartition(1)
-        .saveAsTextFile(model_save_dir + "model_" + k.toString + "/model_analysis")
-
-      if (tmpVariance > maxVar) {
-        maxVar = tmpVariance
-        threshold = goodthreshold
-        ret_model = model
-      }
-
-      val trainPrediction: RDD[(String, (Double, Double))] = trainRDD.map(v => {
-        (v._1, (v._2.label, model.predict(v._2.features)))
-      })
-      val correctUid: RDD[(String, Double)] = trainPrediction.filter(v => {
-        (v._2._1 == 1.0 && v._2._2 >= goodthreshold) || v._2._1 == 0.0
-      }).map(v => (v._1, v._2._1))
-      val newTrainRDD: RDD[(String, LabeledPoint)] = correctUid.join(trainRDD).map(v => (v._1, v._2._2)).cache()
-      trainRDD.unpersist()
-      trainRDD.count()
-      trainRDD = newTrainRDD
-      prediction.unpersist()
-      prediction.count()
-      k += 1
-    }
-    println("\n\n@@@@@@@@@@@@@  maxVar: " + maxVar + split + "goodthreshold: " + goodthreshold + "  @@@@@@@@@@@@@@\n\n")
-    (threshold, ret_model)
-  }
-
-  def build_model_1(
+  def build_model(
                    data_set: (RDD[(String, LabeledPoint)], RDD[(String, LabeledPoint)]),
                    classes_num: Int
                  ): (LogisticRegressionModel, RDD[(String, (Double, Double))]) = {
@@ -310,18 +186,6 @@ object SexModelByAppInstall {
 
     val model: LogisticRegressionModel = new LogisticRegressionWithLBFGS().setNumClasses(classes_num).run(train_rdd.map(_._2))
     val valid_result: RDD[(String, (Double, Double))] = valid_rdd.map(v => (v._1, (model.predict(v._2.features), v._2.label)))
-
-    // val multiclassMetrics = new MulticlassMetrics(valid_result.map(_._2))
-    // println("\n\n ********************** Precision = " + multiclassMetrics.precision + " ********************* \n\n")
-    // println("\n\n ********************** Recall = " + multiclassMetrics.recall + " ********************* \n\n")
-    // for (i <- 0 to classes_num-1) {
-    //   println("\n\n ********************** Precision_" + i + " = " + multiclassMetrics.precision(i) + " ********************* \n\n")
-    // }
-
-    // val binary_class_metrics = new BinaryClassificationMetrics(valid_result.map(_._2))
-    // val roc = binary_class_metrics.roc()
-    // val au_roc = binary_class_metrics.areaUnderROC()
-    // println("\n\n ********************** AUROC: " + au_roc + " ********************* \n\n")
 
     return (model, valid_result)
 
--- src/main/scala/SexModel/Validation/Valid_sex_model_performance.scala
+++ src/main/scala/SexModel/Validation/Valid_sex_model_performance.scala
@@ -3,9 +3,7 @@ package SexModel.Validation
 import java.util.Calendar
 import java.text.SimpleDateFormat
 
-import org.apache.spark.sql.functions.avg
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.{SparkConf, SparkContext}
@@ -13,7 +11,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object Valid_sex_model_performance {
 
-  case class Accuracy(accuracy: Double, date: String)
+  case class Accuracy(date: String, accuracy_full: Double, accuracy_part: Double)
 
   def main(args: Array[String]): Unit = {
     val sparkConf: SparkConf = new SparkConf()
@@ -50,57 +48,49 @@ object Valid_sex_model_performance {
 
 
     // validation data set from idcard info which is not included by sex_model dataset
-    val id_card_info_table: String = "algo.yf_age_gender_accord_idcard_v2"
-    val predict_result_table: String = "algo.yf_sex_model_app_install_predict_on_30000_new_2"
-    val select_latest_date_sql = "show PARTITIONS " + id_card_info_table
+    val id_card_info_full_table: String = "algo.yf_age_gender_accord_idcard_v2"
+    val id_card_info_part_table: String = "algo.yf_sex_label_known_and_only_in_idcard"
+    // val predict_result_table: String = "algo.yf_sex_model_app_install_predict_on_30000_new_1"
+    val predict_result_table: String = "algo.sex_model_latest_predictionall"
+
+    val select_latest_date_sql = "show PARTITIONS " + id_card_info_full_table
     val latest_date: String = hiveContext.sql(select_latest_date_sql).map(v => v(0).toString.split("=")(1).toInt).collect().sortWith((a, b) => a > b)(0).toString
-    println("***************** The latest date of id_card_info_table: " + latest_date.toString() + " *************")
+    println("***************** The latest date of id_card_info_full_table: " + latest_date.toString() + " *************")
 
-    val select_id_info_sql: String = "SELECT T1.imei, T1.gender as sex_idcard from " + id_card_info_table + " as T1 where stat_date=" + latest_date
-    val select_predict_SQL: String = "select T2.imei, T2.sex as sex_pred from " + predict_result_table + " as T2"
-    val id_card_info_df: DataFrame = hiveContext.sql(select_id_info_sql)
-    val predict_res_df: DataFrame = hiveContext.sql(select_predict_SQL)
+    val select_id_info_full_sql: String = "SELECT imei, gender as sex_idcard from " + id_card_info_full_table + " where stat_date=" + latest_date
+    val select_id_info_part_sql: String = "select imei, sex as sex_idcard from " + id_card_info_part_table
+    val select_predict_res_sql: String = "select imei, sex as sex_pred from " + predict_result_table
+    val id_card_info_full_df: DataFrame = hiveContext.sql(select_id_info_full_sql)
+    val id_card_info_part_df: DataFrame = hiveContext.sql(select_id_info_part_sql)
+    val predict_res_df: DataFrame = hiveContext.sql(select_predict_res_sql)
 
-    val imei_matched = id_card_info_df.join(predict_res_df, id_card_info_df("imei") === predict_res_df("imei"))
-    val correct_pred = imei_matched.rdd.filter(v => v(1).toString == v(3).toString)
-    val accuracy = correct_pred.count() * 1.0 / imei_matched.count()
+    // evaluation using full data of id card
+    val imei_matched_0 = id_card_info_full_df.join(predict_res_df, id_card_info_full_df("imei") === predict_res_df("imei"))
+    val correct_pred_0 = imei_matched_0.rdd.filter(v => v(1).toString == v(3).toString)
+    val accuracy_0 = correct_pred_0.count() * 1.0 / imei_matched_0.count()
+    println("\n\n***************** Evaluation using full data of id card (accuracy):" + accuracy_0  + "************ \n\n")
 
-    // validation on dataset: yf_sex_label_known_and_only_in_idcard
-    println("\n\n**************************** Starting evaluation *************************** \n\n")
-
-
-
-    val valid_res = resDF.select(avg(($"sex_idcard" === $"sex_pred").cast("integer")))
-    val accuracy: DataFrame = valid_res.map(v => Accuracy(v(0).toString.toDouble, yestoday_Date)).toDF()
-    println("\n\n**************************** Accuracy is: ************************** \n\n")
-    accuracy.show()
-
-
-    // validation on dataset: yf_sex_label_known_and_only_in_idcard
-    println("\n\n**************************** Starting evaluation *************************** \n\n")
-    val selectSQL: String = "select T1.imei, T1.sex as sex_idcard, T2.sex as sex_pred from algo.yf_sex_label_known_and_only_in_idcard as T1 join algo.sex_model_latest_predictionall as T2 on T1.imei = T2.imei"
-    val resDF: DataFrame = hiveContext.sql(selectSQL)
-    val valid_res = resDF.select(avg(($"sex_idcard" === $"sex_pred").cast("integer")))
-    val accuracy: DataFrame = valid_res.map(v => Accuracy(v(0).toString.toDouble, yestoday_Date)).toDF()
-    println("\n\n**************************** Accuracy is: ************************** \n\n")
-    accuracy.show()
-
-    val valid_res_1 = resDF.rdd.filter(v => v(1).toString == v(2).toString).count() * 1.0 / resDF.count()
+    // evaluation using part data of id card
+    val imei_matched_1 = id_card_info_part_df.join(predict_res_df, id_card_info_part_df("imei") === predict_res_df("imei"))
+    val correct_pred_1 = imei_matched_1.rdd.filter(v => v(1).toString == v(3).toString)
+    val accuracy_1 = correct_pred_1.count() * 1.0 / imei_matched_1.count()
+    println("\n\n***************** Evaluation using part data of id card (accuracy):" + accuracy_1  + "************ \n\n")
 
 
     // write the result to table algo.yf_sex_model_performance
+    val accuracy: DataFrame = Seq((accuracy_0, accuracy_1)).map(v => Accuracy(yestoday_Date, v._1, v._2)).toDF()
     accuracy.registerTempTable("accuracy_temp_table")
     println("\n\n**************************** Starting writing table *************************** \n\n")
-    val create_table_name: String = "algo.yf_sex_model_performance_new"
-    val create_table_sql: String = "create table if not exists " +
-                                  create_table_name +
-                                  " (accuracy double, date string) stored as textfile"
+    val performance_table_name: String = "algo.yf_sex_model_performance_new"
+    val create_performance_table_sql: String = "create table if not exists " +
+                                  performance_table_name +
+                                  " (date string, accuracy_full double, accuracy_part double) stored as textfile"
     val insert_table_sql: String =
-      "insert overwrite table " +
-        create_table_name +
+      "insert into table " +
+        performance_table_name +
         " select * from accuracy_temp_table"
 
-    hiveContext.sql(create_table_sql)
+    hiveContext.sql(create_performance_table_sql)
     hiveContext.sql(insert_table_sql)
     println("\n\n**************************** Finished writing table *************************** \n\n")
   }
